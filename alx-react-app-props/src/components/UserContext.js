93e3cb31d9c8bebf5f64ed5b61f24e2053efdd23
AaronBolten <aaronbolten244@gmail.com>
2025-07-23 18:48:49 +0200

    User 2 change

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,5 +1,15 @@
-import { createContext } from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
 
-const UserContext = createContext();
+function UserProfile() {
+  const userData = useContext(UserContext);
 
-export default UserContext;
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
