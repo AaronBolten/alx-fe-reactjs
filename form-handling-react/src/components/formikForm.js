0e806dac1236e0b69b7bcc5d6e18ad6190021572
AaronBolten <aaronbolten244@gmail.com>
2025-08-24 22:06:30 +0200

    form4-change

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,8 +1,8 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-const RegistrationSchema = Yup.object({
-  username: Yup.string().trim().required("Username is required."),
+ export const RegistrationSchema = Yup.object({
+  username: Yup.string().required("Username is required."),
   email: Yup.string().email("Enter a valid email.").required("Email is required."),
   password: Yup.string().min(6, "Min 6 characters.").required("Password is required."),
 });
